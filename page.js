c8886a8b37bbaa874e0c08982c9f24b5facfbadb
Raine Virta <raine.virta@gmail.com>
2013-04-14 01:29:34 +0300

    send refresh .end_of_session enters dom

--- page.js
+++ page.js
@@ -1,3 +1,20 @@
-if (document.location.pathname === '/cave/waypoint/') {
-	chrome.extension.sendRequest('refresh');
-}
+var observer = new window.WebKitMutationObserver(function(mutations, observer) {
+	for (var i = mutations.length - 1; i >= 0; i--){
+		var m = mutations[i];
+
+		for (var j = m.addedNodes.length - 1; j >= 0; j--){
+			var node = m.addedNodes[j];
+			if (node.className && node.className.indexOf('end_of_session') > -1) {
+				chrome.runtime.sendMessage('refresh');
+			}
+		};
+	};
+});
+
+var gardeningArea = document.getElementById('gardening-area');
+
+observer.observe(gardeningArea, {
+  subtree: true,
+  attributes: true,
+  childList: true
+});
