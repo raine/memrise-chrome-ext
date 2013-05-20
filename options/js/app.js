7fcc6749ab3443cc16cfc7d6e96c16f262854ed1
Raine Virta <raine.virta@gmail.com>
2013-05-20 18:34:08 +0300

    Revert "rivets binders test code"
    This reverts commit cffc058b53aa6225834824b2c91e175cf23a7807.

--- options/js/app.js
+++ options/js/app.js
@@ -1,54 +1,5 @@
 var app = app || {};
 
-rivets.binders.test = {
-	// disabled: function() {
-	// 	return !this.view.models.topic.get('enabled')
-	// },
-
-	routine: function() {
-		console.log('routine', arguments, this);
-		el.disabled = !value
-	},
-
-	bind: function(el) {
-		var self = this;
-
-		// this.binder.routine = _.bind(this.binder.routine, this);
-
-		// this.disabled = function() {
-		// 	return !self.view.models.topic.get('enabled')
-		// };
-
-		this.view.models.topic.on('change:enabled', function(model, val) {
-			self.binder.routine(self.el);
-		});
-
-		// var adapter = rivets.config.adapter
-		// var self = this
-
-		// this.callback = function() {
-		// 	currentValue = _.clone(adapter.read(self.model, self.keypath))
-
-		// 	if(el.value && _.contains(currentValue, el.value)) {
-		// 		newValue = _.without(currentValue, el.value)
-		// 		adapter.publish(self.model, self.keypath, newValue)
-		// 	} else {
-		// 		currentValue.push(el.value)
-		// 		adapter.publish(self.model, self.keypath, currentValue)
-		// 	}
-		// }
-
-		// $(el).on('change', this.callback)
-	},
-
-	unbind: function(el) {
-		console.log('unbind');
-		this.view.models.topic.off('change:enabled');
-	}
-};
-
-// _.bindAll(rivets.binders.test);
-
 rivets.configure({
 	adapter: {
 		subscribe: function(obj, keypath, callback) {
