30c66395c48f943260bf7123b3846eb8213adc5c
Raine Virta <raine.virta@gmail.com>
2013-05-21 22:21:48 +0300

    hide rivets configuration

--- options/js/app.js
+++ options/js/app.js
@@ -1,57 +1,5 @@
 var app = app || {};
 
-rivets.binders['disable-if'] = {
-	routine: function(el, courses) {
-		el.disabled = courses.every(function(c) {
-			return !c.get('enabled');
-		});
-	},
-
-	bind: function(el) {
-		var self = this;
-
-		this.change = function() {
-			self.binder.routine(self.el, this.get('courses'));
-		};
-
-		this.model.on('change', this.change);
-	},
-
-	unbind: function() {
-		this.model.off('change', this.change);
-	}
-};
-
-rivets.configure({
-	adapter: {
-		subscribe: function(obj, keypath, callback) {
-			obj.on('change:' + keypath, callback);
-		},
-
-		unsubscribe: function(obj, keypath, callback) {
-			obj.off('change:' + keypath, callback);
-		},
-
-		read: function(obj, keypath) {
-			return obj.get(keypath);
-		},
-
-		publish: function(obj, keypath, value) {
-			obj.set(keypath, value);
-		}
-	}
-});
-
-rivets.formatters.number = {
-	read: function(value) {
-		return value;
-	},
-
-	publish: function(value) {
-		return +value;
-	}
-};
-
 app.WhatsNewView = Marionette.ItemView.extend({
 	initialize: function() {
 		this.on('fetch', this.render);
