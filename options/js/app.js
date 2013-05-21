b855c87e808c3c283faddd5f5ab66210d71ebeea
Raine Virta <raine.virta@gmail.com>
2013-05-21 23:15:35 +0300

    rename App to Options

--- options/js/app.js
+++ options/js/app.js
@@ -3,19 +3,19 @@ var app = app || {};
 (function($) {
 	'use strict';
 
-	app.App = new Marionette.Application();
-	app.App.addRegions({
+	app.Options = new Marionette.Application();
+	app.Options.addRegions({
 		content: '#content'
 	})
 
-	app.App.addInitializer(function(options) {
+	app.Options.addInitializer(function(options) {
 		var whatsnew = new app.Changes();
 		var settings = new app.SettingsView();
 
-		app.App.content.show(settings);
+		app.Options.content.show(settings);
 	});
 })(jQuery);
 
 $(function() {
-	app.App.start();
+	app.Options.start();
 });
