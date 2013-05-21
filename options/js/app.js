625b50933cec9266df2dc97a09ed94f8a4a441bd
Raine Virta <raine.virta@gmail.com>
2013-05-21 23:14:22 +0300

    put changes view in its own file

--- options/js/app.js
+++ options/js/app.js
@@ -3,34 +3,13 @@ var app = app || {};
 (function($) {
 	'use strict';
 
-	app.WhatsNewView = Marionette.ItemView.extend({
-		initialize: function() {
-			this.on('fetch', this.render);
-
-			this.fetch(function(data) {
-				this.data = data;
-				this.trigger('fetch');	
-			}.bind(this));
-		},
-
-		fetch: function(cb) {
-			$.get(chrome.extension.getURL('CHANGES.html'), function(data) {
-				cb(data);
-			});
-		},
-
-		render: function() {
-			this.$el.html(this.data);
-		}
-	});
-
 	app.App = new Marionette.Application();
 	app.App.addRegions({
 		content: '#content'
 	})
 
 	app.App.addInitializer(function(options) {
-		var whatsnew = new app.WhatsNewView();
+		var whatsnew = new app.Changes();
 		var settings = new app.SettingsView();
 
 		app.App.content.show(settings);
---
+++ options/js/changes.view.js
@@ -0,0 +1,26 @@
+var app = app || {};
+
+(function($) {
+	'use strict';
+
+	app.Changes = Marionette.ItemView.extend({
+		initialize: function() {
+			this.on('fetch', this.render);
+
+			this.fetch(function(data) {
+				this.data = data;
+				this.trigger('fetch');	
+			}.bind(this));
+		},
+
+		fetch: function(cb) {
+			$.get(chrome.extension.getURL('CHANGES.html'), function(data) {
+				cb(data);
+			});
+		},
+
+		render: function() {
+			this.$el.html(this.data);
+		}
+	});
+})(jQuery);
