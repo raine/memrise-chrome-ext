b181b30a21ae4f70443f307b5f3bc414c9f78e15
Raine Virta <raine.virta@gmail.com>
2013-05-25 16:21:12 +0300

    separate localstore from settings.js

--- options/js/settings.js
+++ options/js/settings.js
@@ -7,62 +7,6 @@ var OPTIONS_DEFAULTS = {
 };
 
 (function() {
-	var ROOT_PREFIX = 'ls.';
-
-	var LocalStore = this.LocalStore = function(prefix) {
-		this.prefix = ROOT_PREFIX + prefix + '.';
-	};
-
-	LocalStore.prototype.writeObj = function(obj) {
-		for (var key in obj) {
-			if (Object.prototype.hasOwnProperty.call(obj, key)) {
-				this.set(key, obj[key]);
-			}
-		}
-	};
-
-	LocalStore.prototype.read = function() {
-		var values = {};
-
-		for (var i = (localStorage.length - 1); i >= 0; i--) {
-			if (localStorage.key(i).indexOf(this.prefix) === 0) {
-				var key = localStorage.key(i).substring(this.prefix.length);
-				var value = this.get(key);
-				if (value !== undefined) {
-					values[key] = value;
-				}
-			}
-		}
-
-		return values;
-	};
-
-	LocalStore.prototype.set = function(key, value) {
-		try {
-			value = JSON.stringify(value);
-		} catch (e) {
-			value = null;
-		}
-
-		localStorage.setItem(this.prefix + key, value);
-	};
-
-	LocalStore.prototype.get = function(key) {
-		var value = localStorage[this.prefix + key];
-		if (value === null) { return undefined; }
-		try { return JSON.parse(value); } catch (e) { return null; }
-	};
-
-	LocalStore.prototype.clear = function() {
-		for (var i = (localStorage.length - 1); i >= 0; i--) {
-			if (localStorage.key(i).substring(0, this.prefix.length) === this.prefix) {
-				localStorage.removeItem(localStorage.key(i));
-			}
-		}
-	};
-}());
-
-(function() {
 	'use strict';
 
 	app.Settings = Backbone.Model.extend({
