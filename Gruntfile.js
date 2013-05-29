cc7c24f865c19214b5a4a24724e947fa42299ffa
Raine Virta <raine.virta@gmail.com>
2013-05-29 18:25:20 +0300

    gruntfile: add grunt-contrib-watch back

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,6 +97,7 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
