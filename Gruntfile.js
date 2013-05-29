09101f3e0e774d3e5c1c5793dccf90d3c4d19f96
Raine Virta <raine.virta@gmail.com>
2013-05-29 18:08:05 +0300

    grunt: concat and minify js

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,12 +33,65 @@ module.exports = function(grunt) {
 				command: 'mocha-phantomjs http://localhost:8080/test/index.html',
 				stdout: true
 			}
-		}
+		},
+
+        concat: {
+            dist: {
+				files: {
+					// Things only used by options
+					'dist/options.concat.js': [
+						'lib/backbone.js',
+						'lib/backbone.marionette.js',
+						'lib/rivets.min.js',
+						'options/js/rivets.cfg.js',
+						'options/js/router.js',
+						'options/js/controller.js',
+						'options/js/topics.js',
+						'options/js/settings.js',
+						'options/js/changes.view.js',
+						'options/js/settings.view.js',
+						'options/js/navigation.view.js',
+						'options/js/app.js'
+					],
+
+					// Libs shared by main and options
+					'dist/common.concat.js': [
+						'shared.js',
+						'lib/localstore.js',
+						'lib/jquery-1.9.1.min.js',
+						'lib/underscore.min.js'
+					],
+
+					// Things only used by main
+					'dist/main.concat.js': [
+						'lib/animation.js',
+						'lib/mixpanel.js',
+						'main.js'
+					]
+				}
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: { }
+            },
+
+            my_target: {
+                files: {
+                    'dist/options.min.js' : ['dist/options.concat.js'],
+                    'dist/common.min.js'  : ['dist/common.concat.js'],
+                    'dist/main.min.js'    : ['dist/main.concat.js']
+                }
+            }
+        },
 	});
 
 	grunt.loadNpmTasks('grunt-exec');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('build', ['concat', 'uglify']);
 	grunt.registerTask('default', function() {
 		var tasks = ['server', 'coffee', 'exec', 'watch'];
 		grunt.option('force', true);
