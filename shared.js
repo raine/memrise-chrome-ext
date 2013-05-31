818f3c518cdcd6b6a3ff92ce3e3220919ce1fa3d
Raine Virta <raine.virta@gmail.com>
2013-05-31 17:23:53 +0300

    shared: implement parseJSON and getCategories to use the API

--- shared.js
+++ shared.js
@@ -8,12 +8,15 @@ var Memrise = {
 	BASE_URL      : 'http://www.memrise.com',
 	DASHBOARD_URL : 'http://www.memrise.com/home',
 	LOGIN_URL     : 'http://www.memrise.com/login',
+	API_URL       : 'http://www.memrise.com/api/category/learning/?with_num_ready_to_water',
 
-	getDB: function(callback) {
+	_get: function(url, callback) {
 		var cb;
-		var url = this.DASHBOARD_URL;
 		if (DEV_ENV) {
-			url = chrome.extension.getURL('/assets/home.html');
+			if (url === this.API_URL) {
+				url = chrome.extension.getURL('/assets/learning.json');
+			}
+
 			cb = function() {
 				var args = arguments;
 				setTimeout(function() {
@@ -25,69 +28,19 @@ var Memrise = {
 		return $.get(url, cb || callback);
 	},
 
-	parseHTML: function(html) {
-		if (html.search(/'is_authenticated': false/) >= 0) {
-			return 'not-logged-in';
-		}
+	getCategories: function(cb) {
+		Memrise._get(this.API_URL, function(json) {
+			Memrise.parseJSON(json, cb);
+		});
+	},
 
-		html = html.replace(/<img\b[^>]*\/>/ig,'');
-		var $html  = $($.parseHTML(html));
-		var groups = [];
-
-		// .whitebox is a single group of courses, like "Animals"
-		$html.find('.container-main .page .whitebox').each(function() {
-			var group = {
-				name: $('.groupname', this).text(),
-				wilting: 0,
-				courses: []
-			};
-
-			// Create slug from the name
-			group.slug = group.name
-				.toLowerCase()
-				.replace(/[^a-z\s]*/g, '')
-				.replace(/\s+/, '-');
-
-			var m, href, btn = $('.group-header .btn', this);
-			if (href = btn.attr('href')) {
-				group.waterPath = href;
-
-				if (m = btn.text().match(/Water\s*\(?(\d+)\)?/)) {
-					group.wilting = parseInt(m[1]);
-				}
-			}
-
-			$('.course-box-wrapper', this).each(function() {
-				var course = {
-					name: $('a.inner-wrap', this).attr('title'),
-					id: parseInt($('.course-progress-box', this).attr('data-course-id')),
-					wilting: 0
-				};
-
-				// TODO: you could combine these because both can't be active
-				var $water = $('.cta-wrap .btn[href*="water"]', this);
-				if ($water.length > 0) {
-					course.waterPath = $water.attr('href');
-
-					if (m = $water.text().match((/Water\s*\(?(\d+)\)?/))) {
-						course.wilting = parseInt(m[1]);
-					}
-				}
-
-				var $harvest = $('.ctn-wrap btn[href*="harvest"]', this);
-				if ($harvest.length > 0) {
-					course.harvestPath = $harvest.attr('href');
-
-					// Make it easier to check which groups have harvestable
-					group.harvestable = true;
-				}
-
-				group.courses.push(course);
+	parseJSON: function(json, cb) {
+		var categories = JSON.parse(json).categories.map(function(c) {
+			return _.extend(_.pick(c, 'name', 'slug'), {
+				wilting: c.num_ready_to_water
 			});
-
-			groups.push(group);
 		});
 
-		return groups;
+		cb(categories);
 	}
 };
