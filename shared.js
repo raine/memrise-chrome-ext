88747fc20835ccef91c72396ac73fabea473c78f
Raine Virta <raine.virta@gmail.com>
2013-06-01 20:08:07 +0300

    shared: wrap Memrise with an anonymous function

--- shared.js
+++ shared.js
@@ -4,48 +4,50 @@ var OPTIONS_DEFAULTS = {
 	"track-usage": true
 };
 
-var USE_FIXTURE = false;
+var Memrise = (function() {
+	var USE_FIXTURE = false;
 
-var Memrise = {
-	BASE_URL      : 'http://www.memrise.com',
-	DASHBOARD_URL : 'http://www.memrise.com/home',
-	LOGIN_URL     : 'http://www.memrise.com/login',
-	API_URL       : 'http://www.memrise.com/api/category/learning/?with_num_ready_to_water',
+	return {
+		BASE_URL      : 'http://www.memrise.com',
+		DASHBOARD_URL : 'http://www.memrise.com/home',
+		LOGIN_URL     : 'http://www.memrise.com/login',
+		API_URL       : 'http://www.memrise.com/api/category/learning/?with_num_ready_to_water',
 
-	_get: function(url, success, error) {
-		if (USE_FIXTURE) {
-			if (url === this.API_URL) {
-				url = chrome.extension.getURL('/assets/learning.json');
+		_get: function(url, success, error) {
+			if (USE_FIXTURE) {
+				if (url === this.API_URL) {
+					url = chrome.extension.getURL('/assets/learning.json');
+				}
 			}
-		}
 
-		return $.ajax(url, {
-			success  : success,
-			error    : error,
-			dataType : 'json'
-		});
-	},
-
-	getCategories: function(cb) {
-		return Memrise._get(this.API_URL, function(data) {
-			Memrise.parseLearningJSON(data, cb);
-		}, function(xhr) {
-			if (xhr.status === 403) {
-				cb('not-logged-in');
-			} else {
-				cb('error');
-			}
-		});
-	},
-
-	parseLearningJSON: function(json, cb) {
-		var categories = json.categories.map(function(c) {
-			return _.extend(_.pick(c, 'name', 'slug'), {
-				wilting: c.num_ready_to_water,
-				courses: []
+			return $.ajax(url, {
+				success  : success,
+				error    : error,
+				dataType : 'json'
 			});
-		});
+		},
 
-		cb(null, categories);
-	}
-};
+		getCategories: function(cb) {
+			return Memrise._get(this.API_URL, function(data) {
+				Memrise.parseLearningJSON(data, cb);
+			}, function(xhr) {
+				if (xhr.status === 403) {
+					cb('not-logged-in');
+				} else {
+					cb('error');
+				}
+			});
+		},
+
+		parseLearningJSON: function(json, cb) {
+			var categories = json.categories.map(function(c) {
+				return _.extend(_.pick(c, 'name', 'slug'), {
+					wilting: c.num_ready_to_water,
+					courses: []
+				});
+			});
+
+			cb(null, categories);
+		}
+	};
+})();
