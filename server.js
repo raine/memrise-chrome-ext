4a55699f67ba18aaba15974317a3977b377058e6
Raine Virta <raine.virta@gmail.com>
2013-06-02 23:23:07 +0300

    server: listen 8080 if executed directly (not required)

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+var _       = require('underscore')
 var connect = require('connect');
-var http = require('http');
+var http    = require('http');
 var app;
 
 app = connect()
-  .use('/node_modules', connect.static('node_modules'))
-  .use('/test', connect.static('test/'))
-  .use('/lib', connect.static('lib/'))
-  .use('/options', connect.static('options/'))
-  .use(connect.static(__dirname));
+	.use('/node_modules', connect.static('node_modules'))
+	.use('/test', connect.static('test/'))
+	.use('/lib', connect.static('lib/'))
+	.use('/options', connect.static('options/'))
+	.use(connect.static(__dirname));
 
 module.exports = http.createServer(app);
+
+if (_.include(process.argv, __filename)) {
+	console.log('Started web server on port 8080');
+	app.listen(8080);
+}
