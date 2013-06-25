abd4173817221dd4c0838e066bd1386e9a6b3013
Raine Virta <raine.virta@gmail.com>
2013-06-25 15:30:47 +0300

    add a default shortcut (Alt+M) for activating the button

--- options/js/settings.view.js
+++ options/js/settings.view.js
@@ -9,21 +9,24 @@ var app = app || {};
 
 		initialize: function() {
 			this.whitelist = new app.TopicsWhitelist();
-			this.stats = new app.Stats();
+			this.stats     = new app.Stats();
+			this.hotkeyP   = this.getHotkey();
 		},
 
 		events: {
 			'click #refresh'           : 'refresh',
 			'click #reset'             : 'reset',
 			'click #test-notification' : 'testNotification',
-			'click .plants'            : 'plantsClick'
+			'click .plants'            : 'plantsClick',
+			'click .go-to-ext'         : 'goToExtensions'
 		},
 
 		ui: {
 			'topics'      : 'section#topics',
 			'stats'       : 'section#stats .content',
 			'description' : '.description',
-			'plants'      : '#notifications .plants'
+			'plants'      : '#notifications .plants',
+			'hotkey'      : 'section#hotkey .hotkey'
 		},
 
 		reset: function() {
@@ -32,6 +35,9 @@ var app = app || {};
 
 		onRender: function() {
 			this.delegateEvents();
+
+			this.hotkeyP.done(this.renderHotkey.bind(this));
+
 			this.whitelist.setElement(this.ui.topics);
 			this.whitelist.render();
 			this.ui.stats.append(this.stats.render().el);
@@ -62,6 +68,29 @@ var app = app || {};
 
 		plantsClick: function() {
 			this.$('.wilting-threshold').focus();
+		},
+
+		getHotkey: function() {
+			var df = Q.defer();
+
+			chrome.commands.getAll(function(hotkeys) {
+				df.resolve(hotkeys[0].shortcut); // _execute_browser_action
+			});
+
+			return df.promise;
+		},
+
+		renderHotkey: function(hotkey) {
+			if (hotkey.length > 0) {
+				this.ui.hotkey.text(hotkey);
+				this.$('.hotkey-on').show();
+			} else {
+				this.$('.hotkey-off').show();
+			}
+		},
+
+		goToExtensions: function() {
+			return chrome.tabs.create({ 'url': 'chrome://extensions' });
 		}
 	});
 })(jQuery);
